2b58ea5efddaa25604ba9195c2d9db90e1d138b1
KhangThinh <aoisyaoran@gmail.com>
2018-03-09 17:51:42 +0700

    bai1

---
+++ Mang01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mang01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String strHostName = Dns.GetHostName();
+            Console.WriteLine("Host Name : " + strHostName);
+
+            IPHostEntry iphostentry = Dns.Resolve(strHostName);
+
+            int nIP = 0;
+            foreach (IPAddress ipaddress in iphostentry.AddressList)
+            {
+                Console.WriteLine("IP #" + ++nIP + ": " +
+                      ipaddress.ToString());
+            }
+        }
+    }
+}
